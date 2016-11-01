edac631a2a784e6f2b8674fd181c24189fe7293d
Erin Bond <erin@erinebond.com>
2016-10-31 18:36:31 -0700

    Recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'recipes/search'
+
+  get 'recipes/index'
+
+  get 'recipes/show'
+
 end
+
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
