b070adf5d246969619799aac3c3c88d4900d85cc
Erin Bond <erin@erinebond.com>
2016-11-01 11:18:02 -0700

    Index page now has a search bar

--- db/migrate/20161101021559_create_recipes.rb
+++ db/migrate/20161101021559_create_recipes.rb
@@ -3,9 +3,9 @@ class CreateRecipes < ActiveRecord::Migration[5.0]
     create_table :recipes do |t|
       t.string :label
       t.string :url
-      t.array :ingredients
-      t.array :health_labels
-      t.array :diet_labels
+      t.string :ingredients
+      t.string :health_labels
+      t.string :diet_labels
       t.string :image
       t.string :summary
       t.integer :yield
