a82879e29043a6c7eac898572e0a6ab844495cf9
Erin Bond <erin@erinebond.com>
2016-11-04 11:41:19 -0700

    Homepage styling, search bar still needs works...

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,10 @@
-
-
 class RecipesController < ApplicationController
 
   def index
     if !params[:q].blank?
-        @recipes = paginate EdamamApiWrapper.search(params[:q], 0, 100), per_page: 10 
+        @recipes = paginate EdamamApiWrapper.search(params[:q], 0, 100), per_page: 10
     end
   end
-
-
 
   def show
     @recipe = EdamamApiWrapper.show_details(params[:uri])
